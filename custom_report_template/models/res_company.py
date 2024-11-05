135abedffb7f98315a61aeabbf3bdb098c5dfb57
jamsheenakc <jamsheena384@gmail.com>
2024-11-05 23:15:59 +0400

    5/11

--- custom_report_template/models/res_company.py
+++ custom_report_template/models/res_company.py
@@ -8,14 +8,14 @@ class ResCompany(models.Model):
     name_in_report_ar = fields.Char(string='Name in Report Arabic',)
     name_in_report_en = fields.Char(string='Name in Report English',)
 
-class BaseDocumentLayout(models.TransientModel):
-    _inherit = 'base.document.layout'
-
-    fax = fields.Char(related='company_id.fax', readonly=True)
-    name_in_report_ar = fields.Many2one(related="company_id.name_in_report_ar", readonly=True)
-    name_in_report_en = fields.Many2one(related="company_id.name_in_report_en", readonly=True)
-    street = fields.Many2one(related="company_id.street", readonly=True)
-    zip = fields.Many2one(related="company_id.zip", readonly=True)
+# class BaseDocumentLayout(models.TransientModel):
+#     _inherit = 'base.document.layout'
+#
+#     fax = fields.Char(related='company_id.fax', readonly=True)
+#     name_in_report_ar = fields.Many2one(related="company_id.name_in_report_ar", readonly=True)
+#     name_in_report_en = fields.Many2one(related="company_id.name_in_report_en", readonly=True)
+#     street = fields.Many2one(related="company_id.street", readonly=True)
+#     zip = fields.Many2one(related="company_id.zip", readonly=True)
 
 
 
